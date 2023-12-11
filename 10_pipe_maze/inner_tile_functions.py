8042375af177afb9f5a645538457ebfb2843a24c
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-12-11 13:42:06 -0800

    Refactor Day 10

--- 10_pipe_maze/inner_tile_functions.py
+++ 10_pipe_maze/inner_tile_functions.py
@@ -3,40 +3,37 @@ from loop_functions import determine_start_piece_shape, Loop
 
 
 def count_inner_tiles_in_grid(grid: PipeGrid, loop: Loop) -> int:
-    inner_tiles_in_rows = []
-    for y in range(grid.height):
-        inner_tiles_in_rows.extend(get_inner_tiles_in_row(grid, loop, y))
-
     inner_tiles = []
-    for x in range(grid.width):
-        inner_tiles_in_column = get_inner_tiles_in_column(grid, loop, x)
-        inner_tiles.extend(tile for tile in inner_tiles_in_column if tile in inner_tiles_in_rows)
+    for y in range(grid.height):
+        inner_tiles.extend(get_inner_tiles_in_row(grid, loop, y))
 
     return len(inner_tiles)
 
 
+# Use scanline fill technique by keeping track of odd vs. even # of intersections
 def get_inner_tiles_in_row(grid: PipeGrid, loop: Loop, y: int) -> list[GridPos]:
+    start_shape = determine_start_piece_shape(loop)
+    loop = [piece for piece in loop if piece[1] == y]  # Only look at pieces that matter
+
     intersections = 0
-    row = grid.rows[y]
-    loop_pieces_in_row = [piece for piece in loop if piece[1] == y]
     inner_tiles = []
-    enter_i = -1
+    enter_inner_x = -1
     last_vertical_char = ''
-    for x in range(len(row)):
+
+    for x in range(grid.width):
         pos = (x, y)
-        if pos not in loop_pieces_in_row:
+        if pos not in loop:
             continue
 
         char = grid.get_char(pos)
         if char == 'S':
-            char = determine_start_piece_shape(loop)
+            char = start_shape
 
         if char == '-':
             continue
 
         if char == '|':
             intersections += 1
-
         elif char == 'J' and last_vertical_char == 'F':
             intersections += 1
         elif char == '7' and last_vertical_char == 'L':
@@ -46,49 +43,10 @@ def get_inner_tiles_in_row(grid: PipeGrid, loop: Loop, y: int) -> list[GridPos]:
             last_vertical_char = char
 
         if char in ['|', 'J', '7'] and intersections % 2 != 0:
-            enter_i = x
-        elif enter_i > 0 and char in ['|', 'L', 'F']:
-            for i in range(enter_i + 1, x):
+            enter_inner_x = x
+        elif enter_inner_x > 0 and char in ['|', 'L', 'F']:
+            for i in range(enter_inner_x + 1, x):
                 inner_tiles.append((i, y))
-            enter_i = -1
-
-    return inner_tiles
-
-
-def get_inner_tiles_in_column(grid: PipeGrid, loop: Loop, x: int) -> list[GridPos]:
-    intersections = 0
-    loop_pieces_in_column = [piece for piece in loop if piece[0] == x]
-    inner_tiles = []
-    enter_i = -1
-    last_horizontal_char = ''
-    for y in range(grid.height):
-        pos = (x, y)
-        if pos not in loop_pieces_in_column:
-            continue
-
-        char = grid.get_char(pos)
-        if char == 'S':
-            char = determine_start_piece_shape(loop)
-
-        if char == '|':
-            continue
-
-        if char == '-':
-            intersections += 1
-
-        elif char == 'J' and last_horizontal_char == 'F':
-            intersections += 1
-        elif char == 'L' and last_horizontal_char == '7':
-            intersections += 1
-
-        if char != '-':
-            last_horizontal_char = char
-
-        if char in ['-', 'J', 'L'] and intersections % 2 != 0:
-            enter_i = y
-        elif enter_i > 0 and char in ['-', 'F', '7']:
-            for i in range(enter_i + 1, y):
-                inner_tiles.append((x, i))
-            enter_i = -1
+            enter_inner_x = -1
 
     return inner_tiles
