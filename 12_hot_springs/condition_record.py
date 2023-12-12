de43f17e4c5b3277acc63e64d3f1916a60910f89
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-12-12 15:22:40 -0800

    Day 12 Part 2

--- 12_hot_springs/condition_record.py
+++ 12_hot_springs/condition_record.py
@@ -1,3 +1,6 @@
+from functools import lru_cache
+
+
 class ConditionRecord:
     OPERATIONAL_CHAR = '.'
     DAMAGED_CHAR = '#'
@@ -7,6 +10,7 @@ class ConditionRecord:
         self.spring_row = spring_row
         self.damaged_contiguous_groups = damaged_contiguous_groups
 
+    @lru_cache(maxsize=None)
     def calc_n_arrangements(self, start_spring_i=0, group_i=0):
         if start_spring_i >= len(self.spring_row) or group_i >= len(self.damaged_contiguous_groups):
             return 0
@@ -21,30 +25,36 @@ class ConditionRecord:
         if remaining_space_required > remaining_space:
             return 0
 
-        # Now to figure out if the current group fits
+        # Now to figure out if the current group matches
         current_group_length = self.damaged_contiguous_groups[group_i]
         end_spring_i = start_spring_i + current_group_length
-        could_fit_here = self.OPERATIONAL_CHAR not in self.spring_row[start_spring_i:end_spring_i]
-        could_fit_here = could_fit_here and (
-                end_spring_i == len(self.spring_row) or self.spring_row[end_spring_i] != self.DAMAGED_CHAR)
-        could_fit_here = could_fit_here and (
-                start_spring_i == 0 or self.spring_row[start_spring_i - 1] != self.DAMAGED_CHAR)
 
-        n_with_starting_one_later = 0
+        fits = self.OPERATIONAL_CHAR not in self.spring_row[start_spring_i:end_spring_i]
+        fits = fits and (end_spring_i == len(self.spring_row) or self.spring_row[end_spring_i] != self.DAMAGED_CHAR)
+        fits = fits and (start_spring_i == 0 or self.spring_row[start_spring_i - 1] != self.DAMAGED_CHAR)
+
+        n_with_starting_more_right = 0
         if self.spring_row[start_spring_i] != self.DAMAGED_CHAR:
-            n_with_starting_one_later = self.calc_n_arrangements(start_spring_i + 1, group_i)
+            n_with_starting_more_right = self.calc_n_arrangements(start_spring_i + 1, group_i)
 
-        if could_fit_here:
+        if fits:
             if n_groups_left_to_fit == 1 and self.DAMAGED_CHAR not in self.spring_row[end_spring_i:]:
-                return 1 + n_with_starting_one_later
+                return 1 + n_with_starting_more_right
         else:
-            return n_with_starting_one_later
+            return n_with_starting_more_right
 
         n_with_this_one_fitting = self.calc_n_arrangements(end_spring_i + 1, group_i + 1)
-        return n_with_starting_one_later + n_with_this_one_fitting
+        return n_with_starting_more_right + n_with_this_one_fitting
 
     @classmethod
-    def from_line(cls, line: str) -> 'ConditionRecord':
+    def from_line(cls, line: str, n_copies=1) -> 'ConditionRecord':
         spring_row, damaged_contiguous_groups = line.split()
         damaged_contiguous_groups = [int(n) for n in damaged_contiguous_groups.split(',')]
-        return cls(spring_row, damaged_contiguous_groups)
+
+        expanded_row = spring_row
+        expanded_damaged_contiguous_groups = damaged_contiguous_groups[:]
+        for _ in range(n_copies - 1):
+            expanded_row += '?' + spring_row
+            expanded_damaged_contiguous_groups += damaged_contiguous_groups[:]
+
+        return cls(expanded_row, expanded_damaged_contiguous_groups)
