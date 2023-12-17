52213c56581dabb499d6e4f5e14abd33d112dbb2
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-12-17 13:05:22 -0800

    Day 17 part 1

--- 17_clumsy_crucible/part_two.py
+++ 17_clumsy_crucible/part_two.py
@@ -6,7 +6,7 @@ INPUT_FILE_PATH = './inputs/puzzle.txt'
 
 def main():
     city_map = CityMap.from_file(INPUT_FILE_PATH)
-    min_cost = find_min_path_cost((0, 0), (city_map.width - 1, city_map.height - 1), city_map, 0, 3)
+    min_cost = find_min_path_cost((0, 0), (city_map.width - 1, city_map.height - 1), city_map, 4, 10)
     print(f'Min cost: {min_cost}')
 
 
--- 17_clumsy_crucible/pathing.py
+++ 17_clumsy_crucible/pathing.py
@@ -1,49 +1,71 @@
+import heapq
+from dataclasses import dataclass
+from math import inf
 from city_map import CityMap
 
-type pos = tuple[int, int]
 
-MAX_REPEAT_DIRECTION = 3
+@dataclass(frozen=True)
+class Node:
+    pos: tuple[int, int]
+    history: tuple[int, int]
+
+    def __eq__(self, other):
+        return self.pos == other.pos and self.history == other.history
 
 
-def find_min_path_cost(start: pos, end: pos, city_map: CityMap) -> int:
-    search_list = [(start, None)]
-    path_cost_and_straightness: dict[pos, tuple[int, tuple]] = {start: (city_map.get_value(start[0], start[1]), (0, 0))}
+def find_min_path_cost(start: tuple[int, int], end: tuple[int, int], city_map: CityMap, min_repeat=0,
+                       max_repeat=inf) -> int:
+    start_node = Node(start, (0, 0))
+    search_list = [start_node]
+
+    path_cost: dict[Node, int] = {start_node: 0}
 
     while search_list:
-        search_list.sort(key=lambda x: (end[0] - x[0][0]) ** 2 + (end[1] - x[0][1]) ** 2, reverse=True)
-        current, previous = search_list.pop()
-        if current == end:
-            break
+        search_list.sort(key=lambda x: path_cost[x], reverse=True)
+        current = search_list.pop()
+        if current.pos == end:
+            if current.history[0] < min_repeat and current.history[1] < min_repeat:
+                continue
+            return path_cost[current]
 
-        current_straightness = path_cost_and_straightness[current][1]
-        neighbors = city_map.get_surrounding_positions(current[0], current[1])
+        neighbors = city_map.get_surrounding_positions(current.pos[0], current.pos[1])
         for neighbor in neighbors:
-            cost_to = path_cost_and_straightness[current][0] + city_map.get_value(neighbor[0], neighbor[1])
-            # if neighbor in path_cost_and_straightness and path_cost_and_straightness[neighbor][0] <= cost_to:
-            #     continue
+            direction_to = (neighbor[0] - current.pos[0], neighbor[1] - current.pos[1])
 
-            direction = (neighbor[0] - current[0], neighbor[1] - current[1])
-            if not is_valid_direction(current_straightness, direction):
+            if not is_valid_direction(direction_to, current.history, min_repeat, max_repeat):
                 continue
 
-            if (current_straightness[0] != 0 and direction != 0) or (
-                    current_straightness[1] != 0 and direction[1] != 0):
-                neighbor_straightness = (current_straightness[0] + direction[0], 0)
+            if direction_to[0] > 0 and current.history[0] > 0:
+                history = (current.history[0] + 1, 0)
+            elif direction_to[1] > 0 and current.history[1] > 0:
+                history = (0, current.history[1] + 1)
             else:
-                neighbor_straightness = direction
+                history = direction_to
 
-            path_cost_and_straightness[neighbor] = (cost_to, neighbor_straightness)
-            choice = (neighbor, current)
-            if choice not in search_list:
-                search_list.append(choice)
+            neighbor_node = Node(neighbor, history)
+            cost_to = path_cost[current] + city_map.get_value(neighbor[0], neighbor[1])
 
-    return path_cost_and_straightness.get(end, (-1,))[0]
+            if neighbor_node in path_cost and path_cost[neighbor_node] < cost_to:
+                continue
+
+            path_cost[neighbor_node] = cost_to
+            if neighbor_node not in search_list:
+                search_list.append(neighbor_node)
+
+    return -1
 
 
-def is_valid_direction(straightness, direction) -> bool:
-    if (straightness[0] > 0 > direction[0]) or (straightness[0] < 0 < direction[0]):
+def is_valid_direction(direction, history, min_repeat=0, max_repeat=inf) -> bool:
+    if history == (0, 0):
+        return True
+
+    if (direction[0] > 0 > history[0]) or (direction[0] < 0 < history[0]):
         return False
-    if (straightness[1] > 0 > direction[1]) or (straightness[1] < 0 < direction[1]):
+    if (direction[1] > 0 > history[1]) or (direction[1] < 0 < history[1]):
         return False
-    combined = (straightness[0] + direction[0], straightness[1] + direction[1])
-    return combined[0] <= MAX_REPEAT_DIRECTION and combined[1] <= MAX_REPEAT_DIRECTION
+
+    combined = (history[0] + direction[0], history[1] + direction[1])
+    if combined[0] != 0 and combined[1] != 0:  # Turning
+        if abs(history[0]) < min_repeat and abs(history[1]) < min_repeat:
+            return False
+    return combined[0] <= max_repeat and combined[1] <= max_repeat
