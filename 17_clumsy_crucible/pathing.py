09a6960022ce2e868e8723e8118a2aefccc08ccc
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-12-17 14:53:12 -0800

    Day 17 part 2

--- 17_clumsy_crucible/pathing.py
+++ 17_clumsy_crucible/pathing.py
@@ -1,4 +1,5 @@
-import heapq
+from heapdict import heapdict
+from collections import defaultdict
 from dataclasses import dataclass
 from math import inf
 from city_map import CityMap
@@ -16,16 +17,16 @@ class Node:
 def find_min_path_cost(start: tuple[int, int], end: tuple[int, int], city_map: CityMap, min_repeat=0,
                        max_repeat=inf) -> int:
     start_node = Node(start, (0, 0))
-    search_list = [start_node]
+    hd = heapdict()
+    hd[start_node] = 0
 
-    path_cost: dict[Node, int] = {start_node: 0}
+    path_cost = defaultdict(lambda: 1000000000)
+    path_cost[start_node] = 0
 
-    while search_list:
-        search_list.sort(key=lambda x: path_cost[x], reverse=True)
-        current = search_list.pop()
-        if current.pos == end:
-            if current.history[0] < min_repeat and current.history[1] < min_repeat:
-                continue
+    while hd:
+        current, _ = hd.popitem()
+
+        if current.pos == end and (abs(current.history[0]) >= min_repeat or abs(current.history[1]) >= min_repeat):
             return path_cost[current]
 
         neighbors = city_map.get_surrounding_positions(current.pos[0], current.pos[1])
@@ -35,22 +36,19 @@ def find_min_path_cost(start: tuple[int, int], end: tuple[int, int], city_map: C
             if not is_valid_direction(direction_to, current.history, min_repeat, max_repeat):
                 continue
 
-            if direction_to[0] > 0 and current.history[0] > 0:
-                history = (current.history[0] + 1, 0)
-            elif direction_to[1] > 0 and current.history[1] > 0:
-                history = (0, current.history[1] + 1)
+            if abs(direction_to[0]) > 0 and abs(current.history[0]) > 0:
+                history = (current.history[0] + direction_to[0], 0)
+            elif abs(direction_to[1]) > 0 and abs(current.history[1]) > 0:
+                history = (0, current.history[1] + direction_to[1])
             else:
                 history = direction_to
 
-            neighbor_node = Node(neighbor, history)
             cost_to = path_cost[current] + city_map.get_value(neighbor[0], neighbor[1])
+            neighbor_node = Node(neighbor, history)
 
-            if neighbor_node in path_cost and path_cost[neighbor_node] < cost_to:
-                continue
-
-            path_cost[neighbor_node] = cost_to
-            if neighbor_node not in search_list:
-                search_list.append(neighbor_node)
+            if cost_to < path_cost[neighbor_node]:
+                path_cost[neighbor_node] = cost_to
+                hd[neighbor_node] = cost_to
 
     return -1
 
@@ -65,7 +63,8 @@ def is_valid_direction(direction, history, min_repeat=0, max_repeat=inf) -> bool
         return False
 
     combined = (history[0] + direction[0], history[1] + direction[1])
+    furthest = max(abs(combined[0]), abs(combined[1]))
     if combined[0] != 0 and combined[1] != 0:  # Turning
-        if abs(history[0]) < min_repeat and abs(history[1]) < min_repeat:
+        if furthest < min_repeat:
             return False
-    return combined[0] <= max_repeat and combined[1] <= max_repeat
+    return furthest <= max_repeat
