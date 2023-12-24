d6f5e64866794f05867411da816fecffa3891da8
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-12-23 18:49:04 -0800

    Day 22 part 2

--- 22_sand_slabs/main.py
+++ 22_sand_slabs/main.py
@@ -6,8 +6,14 @@ INPUT_FILE_PATH = './inputs/puzzle.txt'
 def main():
     sim = SandSimulation.from_file(INPUT_FILE_PATH)
     sim.simulate_falling()
-    n = sim.count_safe_to_disintegrate()
-    print(n)
+
+    # Part 1
+    n_safe_to_disintegrate = sim.calc_safe_to_disintegrate()
+    print(f'(Part 1) # of boxes that are safe to disintegrate: {n_safe_to_disintegrate}')
+
+    # Part 2
+    n_affected_by_unsafe = sim.sum_affected_for_each_block_disintegrated()
+    print(f'(Part 2) Sum of affected boxes for each block disintegrated: {n_affected_by_unsafe}')
 
 
 if __name__ == '__main__':
--- 22_sand_slabs/sand_simulation.py
+++ 22_sand_slabs/sand_simulation.py
@@ -6,9 +6,12 @@ class SandSimulation:
     def __init__(self, boxes: list[BoundingBox]):
         self.__boxes = []
         self.__z_layers = defaultdict(lambda: [])
-
         for box in boxes:
             self.add_box(box)
+
+        self.__supported_by = defaultdict(lambda: [])
+        self.__supports = defaultdict(lambda: [])
+        self.__simulated = False
 
     @classmethod
     def from_file(cls, file_path) -> 'SandSimulation':
@@ -27,6 +30,7 @@ class SandSimulation:
         self.__boxes.append(box)
         for z in range(box.bottom, box.top + 1):
             self.__z_layers[z].append(box)
+        self.__simulated = False
 
     def simulate_falling(self):
         self.__boxes.sort(key=lambda x: x.bottom)
@@ -53,9 +57,13 @@ class SandSimulation:
             if new_bottom == -1:
                 continue
 
-            self.move_box_down(box, new_bottom)
+            self.__move_box_down(box, new_bottom)
 
-    def move_box_down(self, box: BoundingBox, new_bottom: int):
+        self.__determine_supported_by()
+        self.__determine_supports()
+        self.__simulated = True
+
+    def __move_box_down(self, box: BoundingBox, new_bottom: int):
         height = box.top - box.bottom + 1
         new_top = new_bottom + height - 1
         top_of_insertion = min(new_top, box.bottom)
@@ -70,9 +78,7 @@ class SandSimulation:
         box.top = new_top
         box.bottom = new_bottom
 
-    def determine_supported_by(self) -> dict[BoundingBox, list[BoundingBox]]:
-        supported_by = defaultdict(lambda: [])
-
+    def __determine_supported_by(self):
         for box in self.__boxes:
             if box.bottom == 1:
                 continue  # Supported by ground
@@ -83,15 +89,24 @@ class SandSimulation:
 
             for other in self.__z_layers[below_z]:
                 if self.do_boxes_horizontally_intersect(box, other):
-                    supported_by[box].append(other)
+                    self.__supported_by[box].append(other)
 
-        return supported_by
+    def __determine_supports(self):
+        for box in self.__boxes:
+            above_z = box.top + 1
+            if above_z not in self.__z_layers:
+                continue
 
-    def count_safe_to_disintegrate(self) -> int:
-        supported_by = self.determine_supported_by()
+            for other in self.__z_layers[above_z]:
+                if self.do_boxes_horizontally_intersect(box, other):
+                    self.__supports[box].append(other)
+
+    def get_unsafe_to_disintegrate(self) -> list[BoundingBox]:
+        assert self.__simulated
+
         unsafe = []
-        for box in supported_by:
-            supporting_boxes = supported_by[box]
+        for box in self.__supported_by:
+            supporting_boxes = self.__supported_by[box]
             n_supporting_boxes = len(supporting_boxes)
             if n_supporting_boxes == 0:
                 continue
@@ -100,7 +115,34 @@ class SandSimulation:
                 if supporting_boxes[0] not in unsafe:
                     unsafe.append(supporting_boxes[0])
 
-        return len(self.__boxes) - len(unsafe)
+        return unsafe
+
+    def calc_safe_to_disintegrate(self) -> int:
+        return len(self.__boxes) - len(self.get_unsafe_to_disintegrate())
+
+    def sum_affected_for_each_block_disintegrated(self):
+        affected_total = 0
+        unsafe = self.get_unsafe_to_disintegrate()
+        for unsafe_box in unsafe:
+            affected = [unsafe_box]
+            to_check = self.__supports[unsafe_box][:]
+            while to_check:
+                above_box = to_check.pop()
+
+                above_supported_by = self.__supported_by[above_box]
+                if any([supported_by not in affected for supported_by in above_supported_by]):
+                    continue
+
+                if above_box not in affected:
+                    affected.append(above_box)
+
+                for b in self.__supports[above_box]:
+                    if b not in affected and b not in to_check:
+                        to_check.append(b)
+
+            affected_total += len(affected) - 1  # -1 because we don't count the disintegrated box
+
+        return affected_total
 
     @staticmethod
     def do_boxes_horizontally_intersect(box1: BoundingBox, box2: BoundingBox):
