beec9753f56600ce81d5b325cd1cfb8a4915dc85
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-12-24 17:24:21 -0800

    Day 23 part 2 rough solution

--- 23_a_long_walk/main.py
+++ 23_a_long_walk/main.py
@@ -1,13 +1,19 @@
 from grid import Grid
-from pathing import find_longest_path
+from pathing import find_longest_path, part_1_can_traverse, part_2_can_traverse
 
 INPUT_FILE_PATH = './inputs/puzzle.txt'
 
 
 def main():
     map_grid = Grid.from_file(INPUT_FILE_PATH)
-    longest = find_longest_path(map_grid, (1, 0), (map_grid.width - 2, map_grid.height - 1))
-    print(f'Longest path: {longest}')
+    start_pos = (1, 0)
+    end_pos = (map_grid.width - 2, map_grid.height - 1)
+
+    longest = find_longest_path(map_grid, start_pos, end_pos, part_1_can_traverse)
+    print(f'(Part 1) Longest path: {longest}')
+
+    longest = find_longest_path(map_grid, start_pos, end_pos, part_2_can_traverse)
+    print(f'(Part 2) Longest path: {longest}')
 
 
 if __name__ == '__main__':
--- 23_a_long_walk/pathing.py
+++ 23_a_long_walk/pathing.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from dataclasses import dataclass
 from grid import Grid
 
 WALL_CHAR = '#'
@@ -11,34 +12,107 @@ WEST_SLOPE_CHAR = '<'
 type Pos = tuple[int, int]
 
 
+@dataclass
 class Path:
-    def __init__(self, positions: list[Pos], previous):
-        self.positions = positions
-        self.previous = previous
+    start: Pos
+    second: Pos
+    end: Pos
+    distance: int
 
 
-def find_longest_path(grid: Grid, start_position: Pos, end_position: Pos):
-    stack = [(start_position, None)]  # Position and previous so we don't double back
-    distances = defaultdict(lambda: -1)
-    distances[start_position] = 0
+def find_longest_path(grid: Grid, start_position: Pos, end_position: Pos, can_traverse_predicate):
+    paths_to_explore: list[tuple[Pos, Pos]] = []  # Stores (prev, start) so goes in right direction
+    explored_paths: dict[Pos, list[Path]] = defaultdict(lambda: [])
+    current_path = [start_position, start_position]
 
-    while stack:
-        pos, prev = stack.pop()
-        distance_to_next = distances[pos] + 1
+    while True:
+        pos = current_path[-1]
         next_positions = grid.get_adjacent_positions(pos[0], pos[1])
-        next_positions = [p for p in next_positions if can_traverse_to_pos(pos, p, grid.get_cell(p[0], p[1]))]
-        next_positions = [p for p in next_positions if p != prev and distance_to_next > distances[p]]
+        next_positions = [p for p in next_positions if can_traverse_predicate(pos, p, grid.get_cell(p[0], p[1]))]
+        next_positions = [p for p in next_positions if p not in current_path]
 
+        if len(next_positions) == 0:
+            # Dead-end
+            if not paths_to_explore:
+                break
+
+            first, second = paths_to_explore.pop()
+            current_path = [first, second]
+            continue
+        elif len(next_positions) == 1:
+            next_pos = next_positions[0]
+            current_path.append(next_pos)
+            if next_pos == end_position:
+                explored_paths[current_path[0]].append(
+                    Path(current_path[0], current_path[1], current_path[-1], len(current_path) - 1))
+                if not paths_to_explore:
+                    break
+
+                first, second = paths_to_explore.pop()
+                current_path = [first, second]
+            continue
+
+        # Split in path
         for next_pos in next_positions:
-            distances[next_pos] = distance_to_next
-            pos_pair = (next_pos, pos)
-            if pos_pair not in stack:
-                stack.append(pos_pair)
+            path_already_explored = False
+            for path in explored_paths[pos]:
+                if next_pos == path.second:
+                    path_already_explored = True
+                    break
+            if path_already_explored:
+                continue
 
-    return distances[end_position]
+            first_two = (pos, next_pos)
+            if first_two in paths_to_explore:
+                continue
+
+            paths_to_explore.append(first_two)
+
+        explored_paths[current_path[0]].append(
+            Path(current_path[0], current_path[1], current_path[-1], len(current_path) - 1))
+        if not paths_to_explore:
+            break
+
+        first, second = paths_to_explore.pop()
+        current_path = [first, second]
+
+    longest_distance = find_longest_path_combination(explored_paths, start_position, end_position)
+    return longest_distance
 
 
-def can_traverse_to_pos(from_pos: Pos, to_pos: Pos, to_char: str):
+def find_longest_path_combination(paths: dict[Pos, list[Path]], start_position: Pos, end_position: Pos,
+                                  travelled=None) -> int:
+    if travelled is None:
+        travelled = [paths[start_position][0]]
+
+    current_path = travelled[-1]
+    if current_path.end == end_position:
+        distance_sum = 0
+        for path in travelled:
+            distance_sum += path.distance
+        return distance_sum
+
+    longest_distance = -1
+    for path in paths[current_path.end]:
+        if path == current_path or path.start != current_path.end:
+            continue
+
+        is_looping = False
+        for t_path in travelled[:-1]:
+            if t_path.end == path.start:
+                is_looping = True
+                break
+        if is_looping:
+            continue
+
+        path_distance = find_longest_path_combination(paths, start_position, end_position, travelled + [path])
+        if path_distance > longest_distance:
+            longest_distance = path_distance
+
+    return longest_distance
+
+
+def part_1_can_traverse(from_pos: Pos, to_pos: Pos, to_char: str):
     if to_char == WALL_CHAR:
         return False
     if to_char == OPEN_CHAR:
@@ -57,3 +131,7 @@ def can_traverse_to_pos(from_pos: Pos, to_pos: Pos, to_char: str):
         return True
 
     return False
+
+
+def part_2_can_traverse(from_pos: Pos, to_pos: Pos, to_char: str):
+    return to_char != WALL_CHAR
